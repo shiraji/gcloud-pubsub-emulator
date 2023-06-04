3196de8c8de45abbf3ad765dd4dbb086df0fd512
shiraji <isogai.shiraji@gmail.com>
2023-06-04 16:29:10 +0900

    Upload to docker.io/shiraji repo

--- make.sh
+++ make.sh
@@ -3,5 +3,4 @@
 set -e
 
 docker build -t gcloud-pubsub-emulator:latest .
-docker push docker.io/marcelcorso/gcloud-pubsub-emulator:latest
-docker push docker.io/messagebird/gcloud-pubsub-emulator:latest
+docker push docker.io/shiraji/gcloud-pubsub-emulator:latest
